58bb65598b64013d7a0b0dc6e7d08c7b6fda96ee
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-09-30 06:15:33 +0800

    Create Example22.py

---
+++ Chapter4/Example22.py
@@ -0,0 +1,13 @@
+#Chapter4/Example22.py
+#!/usr/bin/python #(说明python解释器)
+#coding:utf-8 #(采用中文编码)
+lv = 1
+print("冒险者你好！你现在一级了。")
+q1 = input("你遇到了一只9级的野生刘宗恺，请问要不要攻击?(Y/N)")
+if q1 = "Y":
+    print("由于刘宗恺太强大,你被打死了。")
+elif q1 = "N": 
+    print("你绕过了刘宗恺，继续往前走。")
+    ...
+else:
+    print("谁叫你乱输的?你死了")
