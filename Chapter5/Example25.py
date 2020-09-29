07aeacddcf55e7b5c44562b2f7ed55a3327fe795
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-09-30 06:38:43 +0800

    Create Example25.py

---
+++ Chapter5/Example25.py
@@ -0,0 +1,9 @@
+#Chapter5/Example25.py
+#!/usr/bin/python
+i=1 #用i表示行数
+   while i ＜＝9 :#共有9行
+    j=1 #用i表示列数
+   while j＜=i: #当列数小于等于行数
+        print("j","*","i","=",j*i) #打印 
+        j=j+1 #令列数自增1
+    i=i+1 #令行数自增1
