36b0c471c57f6eea3466b680a544a625fc8aba00
lzr2006 <65013694+lzr2006@users.noreply.github.com>
2020-09-30 06:51:09 +0800

    Create Example32.py

---
+++ Chapter6/Example32.py
@@ -0,0 +1,12 @@
+#Chapter6/Example32.py
+a = "i am zongzi"
+b = a.isalpha()    #返回的是一个bool值,需要用一个变量存储。
+print(b)
+b = a.isdigit()
+print(b)
+b = a.isspace()
+print(b)
+b = a.islower()
+print(b)
+b = a.isupper()
+print(b)0
